cf92459a9d19ba2f0025a253c8dbe81915e3f1b9
Toshi <waykiyoharu@gmail.com>
2022-03-05 16:21:10 +0900

    画像の移動をスムーズにした

--- script.js
+++ script.js
@@ -5,20 +5,64 @@ let rico = new Object();
 rico.x = 0;
 rico.y = 0;
 
-addEventListener("keydown", keydownfunc);
+
+//キーオブジェクト
+let key = new Object();
+key.up = false;
+key.down = false;
+key.right = false;
+key.left = false;
+
+
+
+
+
+
+
+
+
+
+function main(){
+
+    //キーボードを押した時
+    addEventListener("keydown", keydownfunc); //第３引数がtrueだとlisenerは呼び出しが１回になる
+    //キーボードを離した時
+    addEventListener("keyup", keyupfunc);
+
+	//方向キーが押されている場合（ばあい）は、りこちゃんが移動する
+	if( key.left === true ) rico.x -= 32;//「32」は画像のサイズが32pxでRPGの１マスが32pxである為、このサイズに設定している。	
+	if( key.up === true ) rico.y -= 32;		
+	if( key.right === true ) rico.x += 32;	
+	if( key.down === true ) rico.y += 32;	
+    document.getElementById('rico').style.top = rico.y + "px";
+    document.getElementById('rico').style.left = rico.x + "px";
+
+    requestAnimationFrame(main);
+}
+requestAnimationFrame(main);
+
+
+//ボタンを押した時実行
 function keydownfunc(event){
- if(event.keyCode ===37) rico.x -= 32;
- if(event.keyCode ===38) rico.y -= 32;
- if(event.keyCode ===39) rico.x += 32;
- if(event.keyCode ===40) rico.y += 32;
- document.getElementById('rico').style.top = rico.y + "px";
- document.getElementById('rico').style.left = rico.x + "px";
+    let key_code = event.keyCode
+    if(key_code ===37) key.left = true;
+    if(key_code ===38) key.up = true;
+    if(key_code ===39) key.right = true;
+    if(key_code ===40) key.down = true;
+   }
+   
+
+//ボタンを離した時実行
+function keyupfunc(event){
+    let key_code = event.keyCode
+    if(key_code ===37) key.left = false;
+    if(key_code ===38) key.up = false;
+    if(key_code ===39) key.right = false;
+    if(key_code ===40) key.down = false;
 }
 
 
 
-
-// //オブジェクト作成令
 // let rico = new Object();
 // //name
 // rico.name ='りこ'
